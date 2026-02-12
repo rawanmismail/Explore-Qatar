767d372af4b319e5b4bc8e7e07e16fa09959e0b3
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-02-12 10:08:45 +0400

    Update database.py

--- database.py
+++ database.py
@@ -33,7 +33,7 @@ def init_db():
         user_id INTEGER NOT NULL,
         item_type TEXT NOT NULL,
         item_name TEXT NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES users(id)
+
     )''')
     
     c.execute('''CREATE TABLE IF NOT EXISTS notifications (
